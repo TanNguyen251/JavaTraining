c201890c27b41fa32d0f7afba8585faa7d1a1a5a
Tan Nguyen <tan.nguyen.9225.1@gmail.com>
2021-06-03 12:14:24 -0700

    Book class added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+class Book {
+    private String title;
+    private String author;
+    private int price;
+
+    public Book(String title, String author, int price){
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public void print(){
+        System.out.println ("Book Title: " + title + ", Author: " + author + ", price: " + price);
+    }
+
+}
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Book book1 = new Book("HellowWorld!", "Tan Nguyen", 25);
+        book1.print();
     }
 }
